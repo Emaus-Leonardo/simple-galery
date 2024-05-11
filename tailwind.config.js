cf6d822905466b703b6f69932765033295f2f2eb
Emaus-Leonardo <126693677+Emaus-Leonardo@users.noreply.github.com>
2024-05-11 14:07:37 -0300

    mediaqueries

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'ssm': '425px',
+        'sssm': '375px'
+      }
+    },
   },
   plugins: [],
 }
